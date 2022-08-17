e5ae460928e8e0acd12af53fa4576845efe9cc10
bmotadev <bmotadev@gmail.com>
2022-08-17 17:28:57 -0300

    Estrutura de Repeticao For Each

---
+++ EstruturaDeRepeticaForEach/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDeRepeticaForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Estrutura de Repetição - For Each
+
+            Console.WriteLine("Digite uma sequência de números separados pelo espaço");
+            //0 1 2 3 4 5 6 7 8 9 10
+
+            var numerosTexto = Console.ReadLine();
+
+            var numeros = numerosTexto.Split(' ');
+
+            foreach (var numero in numeros) // string numero in numeros
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
---
+++ EstruturaDeRepeticaoWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaDeRepeticaoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Estrutura de Repetição - While
+            Console.WriteLine("Digite uma sequência de números separados por espaço.");
+            // 0 1 2 3 4 5 6 7 8 9 10
+            var numerosTexto = Console.ReadLine();
+
+            var numeros = numerosTexto.Split(' ');
+
+            var contador = 0;
+
+            while (contador < numeros.Length)
+            {
+                Console.WriteLine(numeros[contador]);
+
+                contador++;
+            }
+
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
